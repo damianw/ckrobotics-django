ce2dffad085fac8e3174ec3f8929963775c0c13c
Damian Wieczorek <damianw@umich.edu>
2013-05-11 05:14:05 -0400

    Robots and teams are linked

--- teams/admin.py
+++ teams/admin.py
@@ -1,15 +1,10 @@
 from teams.models import *
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import User
 
-class TeamAdmin(admin.ModelAdmin):
-  fieldsets = [
-    (None,	{'fields': ['name']}),
-    (None,	{'fields': ['number']}),
-    (None,	{'fields': ['founded_date']}),
-    (None,	{'fields': ['league']})
-  ]
+class TeamAdmin(GroupAdmin):
+  pass
 
 class MemberInline(admin.StackedInline):
   model = Member
@@ -19,7 +14,14 @@ class MemberInline(admin.StackedInline):
 class UserAdmin(UserAdmin):
   inlines = (MemberInline, )
 
+class RobotAdmin(admin.ModelAdmin):
+  fieldsets = [
+      (None,  {'fields': ['name']}),
+      (None,  {'fields': ['description']}),
+  ]
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
+admin.site.register(Robot, RobotAdmin)
 admin.site.register(Team, TeamAdmin)
--- teams/models.py
+++ teams/models.py
@@ -1,24 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from blog.models import LEAGUETYPE
-
-class Team(models.Model):
-  name = models.CharField(max_length=200)
-  number = models.IntegerField(max_length=200)
-  founded_date = models.DateField()
-  league = models.CharField(max_length=1, choices=LEAGUETYPE)
-
-  def __unicode__(self):
-  	return self.name
-
-class Member(models.Model):
-	user = models.OneToOneField(User)
-	teams = models.ManyToManyField(Team)
-	join_date = models.DateField()
-	current = models.BooleanField()
-	grad_year = models.IntegerField()
 
 class Robot(models.Model):
 	name = models.CharField(max_length=50)
 	description = models.CharField(max_length=1000)
-	teams = models.ManyToManyField(Team)
+
+class Team(Group):
+  number = models.IntegerField(max_length=200)
+  founded_date = models.DateField()
+  league = models.CharField(max_length=1, choices=LEAGUETYPE)
+  robots = models.ManyToManyField(Robot)
+
+class Member(models.Model):
+	user = models.OneToOneField(User)
+	join_date = models.DateField()
+	current = models.BooleanField()
+	grad_year = models.IntegerField()
