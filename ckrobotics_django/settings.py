6da726ef8d9cb0557cfa507924c4452cdb7a54a2
Damian Wieczorek <damianw@umich.edu>
2013-05-11 09:54:05 -0400

    Got South working

--- ckrobotics_django/settings.py
+++ ckrobotics_django/settings.py
@@ -126,10 +126,10 @@ INSTALLED_APPS = (
     'storages',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'polls',
+    #'polls',
     'teams',
     'blog',
-    #'south',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
--- teams/models.py
+++ teams/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 
+Group.add_to_class('league', models.BooleanField())
 
 class Robot(models.Model):
 	name = models.CharField(max_length=50)
@@ -15,21 +16,17 @@ class Member(models.Model):
 	grad_year = models.IntegerField()
 
 class Team(models.Model):
-	def save(self, *args, **kwargs):
-		super(Team, self).save(*args, **kwargs)
-		if len(self.groups.filter(name=self.primarygroup.name)) < 1:
-			self.groups.add(Group.objects.get(name=self.primarygroup.name))
-		super(Team, self).save(*args, **kwargs)
 	def __unicode__(self):
 		return self.primarygroup.name + ' ' + str(self.number) + ' ' + str(self.name)
 	name = models.CharField(max_length=50)
 	number = models.IntegerField()
 	founded_date = models.DateField()
+	members = models.ManyToManyField(Member, blank=True, related_name='teams')
 	# league = models.CharField(max_length=1, choices=LEAGUETYPE)
 	primarygroup = models.ForeignKey(Group, related_name='primaryteams')
 	groups = models.ManyToManyField(Group, related_name='teams')
 	robots = models.ManyToManyField(Robot, blank=True)
-Group.add_to_class('league', models.BooleanField())
+
 # Group.add_to_class('team', models.OneToOneField(Team, 
 # 	related_name='group', blank=True, null=True))
 # Group.team.blank=False
